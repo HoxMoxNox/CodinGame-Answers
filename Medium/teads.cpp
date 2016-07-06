bd2e2eeb8c00a5161880433f8b0deab4eea1a5e3
Angel Macias <HoxMoxNox@users.noreply.github.com>
2016-07-06 10:52:10 -0400

    finished

--- Medium/teads.cpp
+++ Medium/teads.cpp
@@ -3,65 +3,143 @@
 #include <vector>
 #include <algorithm>
 #include <map>
-#include<list>
+#include <list>
 
 using namespace std;
 
+// This class represents a directed graph using adjacency list representation
 class Graph
 {
-	int V; // No. of vertices
-	list<int> *adj; // Pointer to an array containing adjacency lists
-	void DFSUtil(int v, bool visited[],int &level,int &maxLevel); // A function used by DFS
+    int V;    // No. of vertices
+    list<int> *adj;    // Pointer to an array containing adjacency lists
 public:
-	Graph(int V); // Constructor
-	void addEdge(int v, int w); // function to add an edge to graph
-	void DFS(int v, int &level,int &maxLevel); // DFS traversal of the vertices reachable from v
+    Graph(int V);  // Constructor
+    void addEdge(int v, int w); // function to add an edge to graph
+    int BFS(int s);  // prints BFS traversal from a given source s
+    int BFS_D(int s,int d); //returns distance from s to d
 };
-
+ 
 Graph::Graph(int V)
 {
-	this->V = V;
-	adj = new list<int>[V];
+    this->V = V;
+    adj = new list<int>[V];
 }
-
+ 
 void Graph::addEdge(int v, int w)
 {
-	adj[v].push_back(w); // Add w to v’s list.
+    adj[v].push_back(w); // Add w to v’s list.
 }
-
-void Graph::DFSUtil(int v, bool visited[], int &level,int &maxLevel)
+ 
+int Graph::BFS(int s)
 {
     
-	// Mark the current node as visited and print it
-	visited[v] = true;
-	
-	if(maxLevel < level)
-	{
-	    maxLevel = level;
-	}
-	
-	// Recur for all the vertices adjacent to this vertex
-	list<int>::iterator i;
-	for (i = adj[v].begin(); i != adj[v].end(); ++i)
-		if (!visited[*i])
-			DFSUtil(*i, visited, ++level,maxLevel);
-			--level;
+    int farthestNode;
+    
+    // Mark all the vertices as not visited
+    bool *visited = new bool[V];
+    for(int i = 0; i < V; i++)
+        visited[i] = false;
+ 
+    // Create a queue for BFS
+    list<int> queue;
+ 
+    // Mark the current node as visited and enqueue it
+    visited[s] = true;
+    queue.push_back(s);
+ 
+    // 'i' will be used to get all adjacent vertices of a vertex
+    list<int>::iterator i;
+ 
+    while(!queue.empty())
+    {
+        // Dequeue a vertex from queue and print it
+        s = queue.front();
+        //cout << s << " ";
+        queue.pop_front();
+        
+        if(queue.empty())
+        {
+            farthestNode = s;
+        }
+ 
+        // Get all adjacent vertices of the dequeued vertex s
+        // If a adjacent has not been visited, then mark it visited
+        // and enqueue it
+        for(i = adj[s].begin(); i != adj[s].end(); ++i)
+        {
+            if(!visited[*i])
+            {
+                visited[*i] = true;
+                queue.push_back(*i);
+            }
+        }
+    }
+    
+    return farthestNode;
+    
 }
 
-// DFS traversal of the vertices reachable from v. It uses recursive DFSUtil()
-void Graph::DFS(int v, int &level,int &maxLevel)
+int Graph::BFS_D(int s,int d)
 {
-	// Mark all the vertices as not visited
-	bool *visited = new bool[V];
-	for (int i = 0; i < V; i++)
-		visited[i] = false;
-	
-	// Call the recursive helper function to print DFS traversal
-	DFSUtil(v, visited, level, maxLevel);
+    //cerr << "S: " << s << " D: " << d << endl;
+    int dist = 0;
+    
+    // Mark all the vertices as not visited
+    bool *visited = new bool[V];
+    for(int i = 0; i < V; i++)
+        visited[i] = false;
+ 
+    // Create a queue for BFS
+    list<int> queue;
+    
+    //distance queue
+    list<int> queueD;
+ 
+    // Mark the current node as visited and enqueue it
+    visited[s] = true;
+    queue.push_back(s);
+    
+    //mark initial distance
+    queueD.push_back(dist);
+ 
+    // 'i' will be used to get all adjacent vertices of a vertex
+    list<int>::iterator i;
+ 
+    while(!queue.empty())
+    {
+        // Dequeue a vertex from queue and print it
+        s = queue.front();
+        //cout << s << " ";
+        queue.pop_front();
+        
+        dist = queueD.front();
+        //cerr << "current dist: " << dist << endl;
+        queueD.pop_front();
+        
+        if(s == d)
+        {
+            return dist;
+        }
+ 
+        // Get all adjacent vertices of the dequeued vertex s
+        // If a adjacent has not been visited, then mark it visited
+        // and enqueue it
+        for(i = adj[s].begin(); i != adj[s].end(); ++i)
+        {
+            if(!visited[*i])
+            {
+                visited[*i] = true;
+                queue.push_back(*i);
+                queueD.push_back(dist+1);
+            }
+        }
+    }
+    
+    return -1; // never found node, shouldn't reach this
+    
 }
 
-
-//Modified the GeeksForGeeks depth first search for this problem
+//Modified the GeeksForGeeks breadth first search for this problem
 
 
 /**
@@ -74,15 +152,9 @@ int main()
     cin >> n; cin.ignore();
     
     Graph g(n*2); 
-	
-	int minMaxLevel = 100000000;
-	
-	//try brute force? seems to fail at test 8
-	map<int,int>edges;
-	map<int,int>::iterator it;
-	
-	int maxConnections = 0;
     
+    int diameter,radius,startingNode,u,v;
+
     for (int i = 0; i < n; i++) {
         int xi; // the ID of a person which is adjacent to yi
         int yi; // the ID of a person which is adjacent to xi
@@ -90,45 +162,21 @@ int main()
         //cerr << xi << " " << yi << endl;
         g.addEdge(xi,yi);
         g.addEdge(yi,xi);
-        edges[xi]++;
-        
-        int currentConnections = edges[xi];
-        
-        if(currentConnections > maxConnections)
-        {
-            maxConnections = currentConnections;
-        }
+        startingNode = xi;
     }
     
+    u = g.BFS(startingNode);
+    v = g.BFS(u); //length between these two is the diameter
+    //cerr << "u: " << u << " v: " << v << endl;
     
-    cerr << "Largest # of connections: " << maxConnections << endl;
-    //cout << 9 << endl;
+    diameter = g.BFS_D(u,v);
+    //cerr << "Diameter: " << diameter << endl;
+    radius = (int)(ceil(diameter/2.0));
+    //cerr << "Radius: " << radius << endl;
+    cout << radius << endl;
     
-    for(it = edges.begin(); it != edges.end(); ++it)
-    {
-        if(it->second >= maxConnections - 2)
-        {
-            int level = 0;
-    	    int maxLevel = 0;
-    	    g.DFS(it->first,level,maxLevel);
-    	    if(maxLevel < minMaxLevel)
-    	    {
-    	        minMaxLevel = maxLevel;
-    	    }    	    
-    	   // if(maxLevel >= 9)
-    	    //{
-    	      //  break;
-    	        //cerr << "Time to propogate from " << it->first << " is " << maxLevel << endl;
-    	        //cerr << it->first << " has " << it->second << " connections" << endl;
-    	        //cerr << endl;
-    	   // }
-
-        }
-	    
-    }
     
-    // The minimal amount of steps required to completely propagate the advertisement
-    cerr << "edges: " << n << endl;
-    cerr << "Min level is " << minMaxLevel << endl;
-    cout << minMaxLevel << endl;
 }
+
+
+
