fa34e163cb7e6f2fae74dd85016f32c484066cb9
Angel Macias <HoxMoxNox@users.noreply.github.com>
2016-07-11 11:49:07 -0400

    Started over
    Fixed angle compensation and turned it into an affine transformation
    added code to predict time to arrive to checkpoint

--- Tournaments/CSB.cpp
+++ Tournaments/CSB.cpp
@@ -3,8 +3,13 @@
 #include <vector>
 #include <algorithm>
 
+# define PI 3.14159265358979323846  /* pi */
+
 using namespace std;
 
+
+pair<int,int> rotateMat(pair<int,int>ship, pair<int,int> checkpoint int angle);
+int turnsToNextCheckpoint(pair<int,int> checkpoint, pair<int,int>ship, int dx, int dy);
 /**
  * Auto-generated code below aims at helping you parse
  * the standard input according to the problem statement.
@@ -16,6 +21,17 @@ int main()
 {
     
     bool boosted = false;
+    bool lapped = false;
+    vector< pair<int,int> > storedCheckpoints;
+    pair<int,int>MID_MAP(8000,4500);
+    
+    //perhaps track acceleration?
+    //int ddy,ddx,pprevX,pprevY;
+    
+    int dx = 0;
+    int dy = 0; // velocity trackers
+    int prevX,prevY;
+    int turn = 0;
             
     // game loop
     while (1) {
@@ -30,62 +46,121 @@ int main()
         int opponentY;
         cin >> opponentX >> opponentY; cin.ignore();
         
+        if(turn > 0)
+        {
+            dx = x - prevX;
+            dy = y  - prevY;
+        }
+        
         int thrust;
+        int epsilon = 2;
+        int delta = 10;
         
-        int epsilon = 5;
+        
+        pair<int,int>currentCheckpoint(nextCheckpointX,nextCheckpointY);
+        pair<int,int> dest(nextCheckpointX, nextCheckpointY);
+        pair<int,int>ship(x,y);
+        
+        int turnsToCP = turnsToNextCheckpoint(currentCheckpoint,ship,dx,dy);
+        
+        double angleToEnemy = atan2(opponentY - y, opponentX - x);
+        double distToEnemy = sqrt( (opponentX - x) * (opponentX - x) + (opponentY - y) * (opponentY - y) );
+        double enemyDistToCheck = sqrt( (nextCheckpointX-opponentX) * (nextCheckpointX-opponentX) + (nextCheckpointY-opponentY) * (nextCheckpointY-opponentY) );
         
         
-        // Write an action using cout. DON'T FORGET THE "<< endl"
-        // To debug: cerr << "Debug messages..." << endl;
-
-
-        // You have to output the target position
-        // followed by the power (0 <= thrust <= 100)
-        // i.e.: "x y thrust"
-        if(nextCheckpointDist <= 2000 && nextCheckpointDist > 600)
-        {
-            thrust = 50;
-        }
-        else if(nextCheckpointDist <= 600)
-        {
-            thrust = 0;
-        }
-        else
-        {
-            thrust = 100;
-        }
         
-        if(nextCheckpointAngle > 90 || nextCheckpointAngle < -90)
-        {
-            thrust = 0;
-        }
-      //  else if(nextCheckpointAngle < 0 + epsilon || nextCheckpointAngle > 0 - epsilon)
-    //    {
-     //       thrust = 100;
-      //  }
-        //cerr << "Current angle: " << nextCheckpointAngle << endl; 
-        if(!boosted && nextCheckpointDist > 6000 && 
-            ((nextCheckpointAngle < 0 + epsilon && nextCheckpointAngle > 0 - epsilon)))
-        {
-            thrust = -1;
-            boosted = true;
-        }
         
-        if(thrust >= 0)
-        {
-            cout << nextCheckpointX << " " << nextCheckpointY << " " << thrust << endl;
-        }
-        else
-        {
-            cout << nextCheckpointX << " " << nextCheckpointY << " BOOST"<< endl;
-        }
-        
+        turn++;
+        prevX = x;
+        prevY = y;
     }
+}
+
+//This uses an AFFINE transformation, not a LINEAR transformation
+pair<int,int> rotateMat(pair<int,int> ship, pair<int,int> checkpoint, int angle)
+{
+    vector<int> rotatedCoords;
+    vector<int> rotatedCheckpoint;
+    vector<int> rotatedShip;
+
+    double rotateMatrix[2][2];
+    rotateMatrix[0][0] = cos( (angle*PI) / 180 );
+    rotateMatrix[0][1] = -sin( (angle*PI) / 180 );
+    rotateMatrix[1][0] = sin( (angle*PI) / 180 );
+    rotateMatrix[1][1] = cos( (angle*PI) / 180 );
+    
+    int x = ship.first;
+    int y = ship.second;
+    
+    cerr << "Ship coords: " << x << " " << y << endl;
+    
+    int cx = checkpoint.first;
+    int cy = checkpoint.second;
+    
+    cerr << "CP coords: " << cx << " " << cy << endl;
+    
+    cerr << "Angle to rotate about: " << angle << endl;
+    
+    cerr << "Rotation Matrix: " << rotateMatrix[0][0] << " " << rotateMatrix[0][1] << endl;
+    cerr << "                 " << rotateMatrix[1][0] << " " << rotateMatrix[1][1] << endl;
+    
+    cerr << "Affine transformation R(X) + Xs - R(Xs)" << endl;
+
+    rotatedCheckpoint.push_back((int)(rotateMatrix[0][0] * cx + rotateMatrix[0][1] * cy));
+    rotatedCheckpoint.push_back((int)(rotateMatrix[1][0] * cx + rotateMatrix[1][1] * cy));
+    
+    cerr << "R(X): " << rotatedCheckpoint[0] << endl;
+    cerr << "      " << rotatedCheckpoint[1] << endl;
+    
+    rotatedShip.push_back((int)(rotateMatrix[0][0] * x + rotateMatrix[0][1] * y));
+    rotatedShip.push_back((int)(rotateMatrix[1][0] * x + rotateMatrix[1][1] * y));
+    
+    cerr << "R(Xs): " << rotatedShip[0] << endl;
+    cerr << "       " << rotatedShip[1] << endl;
+    
+    
+    cerr << "Xs:   " << x << endl;
+    cerr << "      " << y << endl;
+
+
+    rotatedCoords.first = rotatedCheckpoint[0] - rotatedShip[0] + x;
+    rotatedCoords.second = rotatedCheckpoint[1] - rotatedShip[1] + y;
+    
+    cerr << "R(X) + Xs - R(Xs): " << rotatedCoords.first << endl;
+    cerr << "                   " << rotatedCoords.second << endl;
+    
+    return rotatedCoords;
+    
 }
 
 
 
-
+int turnsToNextCheckpoint(pair<int,int> checkpoint, pair<int,int>ship, int dx, int dy)
+{
+    int MAX_TURNS = 30;
+    int CP_RADIUS = 600;
+    int cx = checkpoint.first;
+    int cy = checkpoint.second;
+    int x = ship.first;
+    int y = ship.second;
+    
+    for(int i = 0; i < MAX_TURNS; i++)
+    {
+        if( (x < cx + CP_RADIUS && x > cx - CP_RADIUS) 
+           && (y < cy + CP_RADIUS && y > cy - CP_RADIUS) )
+           {
+               return i;
+           }
+           else
+           {
+               x += dx;
+               y += dy;
+           }
+    }
+    
+    return MAX_TURNS;
+    
+}
 
 
 
